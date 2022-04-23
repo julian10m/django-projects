3645944569ffb551e5c096322d3cc08626bf95ca
julian10m <j.d12f90@gmail.com>
2022-04-23 18:39:09 +0200

    added pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 def post_list(request):
-    posts = Post.published.all()
+    POSTS_PER_PAGE = 3
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, POSTS_PER_PAGE)
+    page = request.GET.get('page') # current page number
+    try:
+        posts = paginator.page(page) # objects for current page
+    except PageNotAnInteger: # if number is weird, then give the first
+        posts = paginator.page(1)
+    except EmptyPage: # if it exceeds the max, then set the last one
+        posts = paginator.page(paginator.num_pages)
     return render(request, 
                  'blog/post/list.html', 
                  {'posts': posts})
