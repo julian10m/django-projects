cc1dcd1be48408d15a9266ffd5417bf12262e6ec
julian10m <j.d12f90@gmail.com>
2022-05-02 15:21:13 +0200

    adding search queries, rank and similarity features

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,8 +3,8 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
-from matplotlib.pyplot import annotate
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from .models import Post, Comment
@@ -99,9 +99,29 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            # 1)
+            # results = Post.published.annotate(
+            #     search=SearchVector('title', 'body'),
+            #     ).filter(search=query)
+            # 2)
+            # search_vector = SearchVector('title', 'body')
+            # search_query = SearchQuery(query)
+            # results = Post.published.annotate(
+            #     search=search_vector,
+            #     rank=SearchRank(search_vector, search_query)
+            #     ).filter(search=search_query).order_by('-rank')
+            # 3)
+            search_vector = SearchVector('title', weight='A') + \
+                SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            # results = Post.published.annotate(
+            #     search=search_vector,
+            #     rank=SearchRank(search_vector, search_query)
+            #     ).filter(rank__gte=0.3).order_by('-rank')
+            #  4)
             results = Post.published.annotate(
-                search=SearchVector('title', 'body'),
-                ).filter(search=query)
+                similarity=TrigramSimilarity('title', query),
+                ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 
                   'blog/post/search.html',
                   {'form': form,
