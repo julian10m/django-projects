55349cd01e6ee8710d29000ae27ed2b2f363c40d
julian10m <j.d12f90@gmail.com>
2022-05-03 11:02:26 +0200

    extended from user to profile model

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     #     name='password_reset_complete'),
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
