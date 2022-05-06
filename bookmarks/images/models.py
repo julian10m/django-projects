1941e2c264f842072b205160656e278430cde099
julian10m <j.d12f90@gmail.com>
2022-05-06 18:55:09 +0200

    adding image detail page

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
+
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              related_name='images_created',
@@ -19,6 +21,10 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('images:detail',
+                        args=[self.id, self.slug])
 
     def __str__(self):
         return self.title
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreateForm
+from .models import Image
 
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request, 
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
 @login_required
 def image_create(request):
     if request.method == 'POST':
@@ -13,7 +20,7 @@ def image_create(request):
             new_item.user = request.user
             new_item.save()
             messages.success(request, 'Image added successfully')
-            return redirect(new_item.get_absolut_url())
+            return redirect(new_item.get_absolute_url())
     else:
         form = ImageCreateForm(data=request.GET)
     return render(request, 
