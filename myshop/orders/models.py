f93ae1449177941dd993251d8f1a3696f62930db
julian10m <j.d12f90@gmail.com>
2022-05-11 10:37:02 +0200

    adding admin view for Orders

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -11,7 +11,7 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
-
+    
     class Meta:
         ordering = ('-created',)
     
