557621637698850c7729868e3b0854153159f425
julian10m <j.d12f90@gmail.com>
2022-05-15 22:20:53 +0200

    extending admin site for Order with custom view

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,13 +1,19 @@
-from urllib import response
 from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
+from urllib import response
 from .models import Order, OrderItem
 import csv
 import datetime
-from django.http import HttpResponse
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f"<a href='{url}'>View</a>")
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -34,7 +40,8 @@ export_to_csv.short_description = 'Export to CSV'
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name',
                     'email', 'address', 'postal_code',
-                    'city', 'paid', 'created', 'updated']
+                    'city', 'paid', 'created', 'updated',
+                    order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
